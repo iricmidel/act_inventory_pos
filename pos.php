c59952f9b1186523950a3f75774ddfab40a17c87
iricmidel <iricmidel@gmail.com>
2018-09-02 18:59:47 +0800

    Updated POS Main Screen

---
+++ pos.php
@@ -0,0 +1,426 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+
+  <title>AC&T Inventory and POS</title>
+
+    <link rel="stylesheet" type="text/css" href="../library/font-awesome/font/font.css">
+    <link rel="stylesheet" type="text/css" href="../library/css/bulma.min.css">
+    <link rel="stylesheet" type="text/css" href="../library/css/sidenav.css">
+    <link rel="stylesheet" type="text/css" href="../library/css/animation.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+  <script src="../library/js/ajax-3.1.1.js"></script>
+  <script src="../library/js/jquery-1.12.4.js"></script>
+</head>
+
+<body>
+
+    <nav id="navbar" class="navbar has-shadow is-spaced">
+      <div class="container">
+        <div class="navbar-item">
+          <div class="title">Arya's Coffee and Tea</div>
+        </div>
+        <div class="navbar-item">
+          <div class="field">
+            <div class="control">
+              <input class="input is-active is-large" type="text" placeholder="Search Products">
+            </div>
+          </div>
+        </div>
+        <div class="navbar-end">
+          <p>Hi $User &ensp;</p>
+
+          <div class ="button is-warning">Logout</div>
+        </div>
+      </div>
+      </nav>
+
+  <div class ="container">
+    <div class = "columns">
+      <div class="column">
+        </br>
+
+        <nav class="breadcrumb" aria-label="breadcrumbs">
+          <ul>
+            <li><a href="#">Top Screen</a></li>
+            <li class="is-active"><a href="#" aria-current="page">Drinks</a></li>
+          </ul>
+        </nav>
+
+        <div class="columns">
+          <div class="column is-one-third">
+            <div class="card" id="showModal">
+              <header class="card-header">
+                <p class="card-header-title">
+                  Caramel Coffee
+                </p>
+                <a href="#" class="card-header-icon" aria-label="more options">
+                  <span class="icon">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                  </span>
+                </a>
+              </header>
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img src="../Assets/images/caramel_coffee.jpeg" alt="Placeholder image">
+                </figure>
+              </div>
+              <footer class="card-footer">
+                <div href="#" class="card-footer-item">Category</div>
+              </footer>
+            </div>
+          </div>
+          <div class="column is-one-third">
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title">
+                  Caramel Coffee
+                </p>
+                <a href="#" class="card-header-icon" aria-label="more options">
+                  <span class="icon">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                  </span>
+                </a>
+              </header>
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img src="../Assets/images/caramel_coffee.jpeg" alt="Placeholder image">
+                </figure>
+              </div>
+              <footer class="card-footer">
+                <div href="#" class="card-footer-item">Category</div>
+              </footer>
+            </div>
+          </div>
+          <div class="column is-one-third">
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title">
+                  Caramel Coffee
+                </p>
+                <a href="#" class="card-header-icon" aria-label="more options">
+                  <span class="icon">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                  </span>
+                </a>
+              </header>
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img src="../Assets/images/caramel_coffee.jpeg" alt="Placeholder image">
+                </figure>
+              </div>
+              <footer class="card-footer">
+                <div href="#" class="card-footer-item">Category</div>
+              </footer>
+            </div>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column is-one-third">
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title">
+                  Caramel Coffee
+                </p>
+                <a href="#" class="card-header-icon" aria-label="more options">
+                  <span class="icon">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                  </span>
+                </a>
+              </header>
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img src="../Assets/images/caramel_coffee.jpeg" alt="Placeholder image">
+                </figure>
+              </div>
+              <footer class="card-footer">
+                <div href="#" class="card-footer-item">Category</div>
+              </footer>
+            </div>
+          </div>
+          <div class="column is-one-third">
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title">
+                  Caramel Coffee
+                </p>
+                <a href="#" class="card-header-icon" aria-label="more options">
+                  <span class="icon">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                  </span>
+                </a>
+              </header>
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img src="../Assets/images/caramel_coffee.jpeg" alt="Placeholder image">
+                </figure>
+              </div>
+              <footer class="card-footer">
+                <div href="#" class="card-footer-item">Category</div>
+              </footer>
+            </div>
+          </div>
+          <div class="column is-one-third">
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title">
+                  Caramel Coffee
+                </p>
+                <a href="#" class="card-header-icon" aria-label="more options">
+                  <span class="icon">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                  </span>
+                </a>
+              </header>
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img src="../Assets/images/caramel_coffee.jpeg" alt="Placeholder image">
+                </figure>
+              </div>
+              <footer class="card-footer">
+                <div href="#" class="card-footer-item">Category</div>
+              </footer>
+            </div>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column is-one-third">
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title">
+                  Caramel Coffee
+                </p>
+                <a href="#" class="card-header-icon" aria-label="more options">
+                  <span class="icon">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                  </span>
+                </a>
+              </header>
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img src="../Assets/images/caramel_coffee.jpeg" alt="Placeholder image">
+                </figure>
+              </div>
+              <footer class="card-footer">
+                <div href="#" class="card-footer-item">Category</div>
+              </footer>
+            </div>
+          </div>
+          <div class="column is-one-third">
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title">
+                  Caramel Coffee
+                </p>
+                <a href="#" class="card-header-icon" aria-label="more options">
+                  <span class="icon">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                  </span>
+                </a>
+              </header>
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img src="../Assets/images/caramel_coffee.jpeg" alt="Placeholder image">
+                </figure>
+              </div>
+              <footer class="card-footer">
+                <div href="#" class="card-footer-item">Category</div>
+              </footer>
+            </div>
+          </div>
+          <div class="column is-one-third">
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title">
+                  Caramel Coffee
+                </p>
+                <a href="#" class="card-header-icon" aria-label="more options">
+                  <span class="icon">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                  </span>
+                </a>
+              </header>
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img src="../Assets/images/caramel_coffee.jpeg" alt="Placeholder image">
+                </figure>
+              </div>
+              <footer class="card-footer">
+                <div href="#" class="card-footer-item">Category</div>
+              </footer>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div class = "column is-one-third order">
+        <div class ="section">
+          </br>
+          <div class = "title">
+            Order List
+          </div>
+          <div class = "subtitle">
+            $Date and Time
+          </div>
+          </br></br>
+          <a href="#" id="showModalStockOut">
+          <h4>Black Coffee 250 ml</h4>
+          </a>
+          <div class = "columns">
+            <div class = "column">
+              <p>@P149.00</p>
+            </div>
+            <div class = "column">
+              <div class="button is-warning is-small">1</div>
+            </div>
+            <div class = "column">
+              <h4>P149.00</h4>
+            </div>
+            <div class = "column">
+              <h4>x</h4>
+            </div>
+          </div>
+
+          <h4>Meatball Pasta</h4>
+          <span class="tag is-warning">
+            Spicy
+          </span>
+          <span class="tag is-warning">
+            No Cheese
+          </span>
+
+          <div class = "columns">
+            <div class = "column">
+              <p>@P170.00</p>
+            </div>
+            <div class = "column">
+              <div class="button is-warning is-small">2</div>
+            </div>
+            <div class = "column">
+              <h4>P340.00</h4>
+            </div>
+            <div class = "column">
+              <h4>x</h4>
+            </div>
+          </div>
+
+          </br>
+          </br>
+          </br>
+
+          <div class = "columns">
+            <div class = "column">
+              <h3>Total:</h3>
+            </div>
+
+            <div class = "column">
+              <h3 class="order-total">P489.00</h3>
+            </div>
+          </div>
+
+          <div class = "columns">
+            <div class = "column">
+              <div class="button is-warning is-medium is-fullwidth">Pay</div>
+            </div>
+          </div>
+
+          <div class = "columns">
+            <div class = "column">
+              <div class="button is-info is-medium is-fullwidth">Save</div>
+            </div>
+            <div class = "column">
+              <div class="button is-danger is-medium is-fullwidth">Discard</div>
+            </div>
+          </div>
+
+
+
+
+        </div>
+    </div>
+  </div>
+
+  <div class="modal">
+    <div class="modal-background"></div>
+    <div class="modal-card animated fadeInDown">
+      <header class="modal-card-head">
+        <p class="modal-card-title level-item has-text-centered">Caramel Coffee</span></p>
+      </header>
+      <section class="modal-card-body">
+        <!-- Content ... -->
+
+        <h4>Variant</h4>
+        <span class="tag is-white is-large">Upsize (30)</span>
+        &nbsp;&nbsp;
+        <span class="tag is-primary is-large">Take Out (30)</span>
+        &nbsp;&nbsp;
+        <span class="tag is-white is-large">Upsize Take Out (15)</span>
+        </br>
+        </br>
+        <span class="tag is-white is-large">Upsize (30)</span>
+        &nbsp;&nbsp;
+        <span class="tag is-white is-large">Take Out (30)</span>
+        &nbsp;&nbsp;
+        <span class="tag is-white is-large">Upsize Take Out (15)</span>
+        </br>
+        </br>
+
+        <h4>Amount</h4>
+
+        <input type="number" class="input-pos">
+
+
+      </section>
+      <footer class="modal-card-foot">
+          <button class="button is-warning">Save changes</button>
+          <button class="button is-danger">Cancel</button>
+      </footer>
+    </div>
+  </div>
+
+  <div class="modal2">
+    <div class="modal-background"></div>
+    <div class="modal-card animated fadeInDown">
+      <header class="modal-card-head">
+        <p class="modal-card-title level-item has-text-centered">Modify Order</p>
+      </header>
+      <section class="modal-card-body">
+        <!-- Content ... -->
+        <h4>Discounts</h4>
+        <span class="tag is-white is-large">10% Discount</span>
+        &nbsp;&nbsp;
+        <span class="tag is-white is-large">20% Discount</span>
+        &nbsp;&nbsp;
+        <span class="tag is-white is-large">100% Discount</span>
+        &nbsp;&nbsp;
+        </br>
+        </br>
+        <h4>Custom Discount</h4>
+        <input type="text" class="input-pos">
+        </br>
+        </br>
+
+        <h4>Show Item in Receipt</h4>
+        <span class="tag is-white is-large">Yes</span>
+        &nbsp;&nbsp;
+        <span class="tag is-white is-large">Hide</span>
+
+      </section>
+      <footer class="modal-card-foot">
+          <button class="button is-warning">Save changes</button>
+          <button class="button is-danger">Cancel</button>
+      </footer>
+    </div>
+  </div>
+
+    <script src="../library/js/sidenav.js"></script>
+    <script src="../library/js/modal.js"></script>
+
+</body>
+
+</html>
